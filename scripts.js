08c35759bb20b39de19b72a9c22b448877cd40dd
dojotech-info <dojo.info@aol.com>
2024-06-17 08:04:36 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,28 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var popupModal = document.getElementById('popupModal');
+
     var calendlyModal = document.getElementById('calendlyModal');
-    var popupModalContent = document.querySelector('#popupModal .modal-content');
+
     var calendlyModalContent = document.querySelector('#calendlyModal .modal-content');
-    var continueButton = document.getElementById('continueButton');
+    
    var closeModal = document.getElementById('closeModal');
     var button = document.querySelector('.button');
     var button1 = document.querySelector('.button1');
 
      calendlyModal.style.display = 'none';
     
-    if (popupModal && continueButton) {
-        continueButton.addEventListener('click', function() {
-            popupModalContent.classList.add('float-out');
-            popupModal.classList.add('fade-out');
-
-            // Wait for the animation to finish before hiding the modal
-            setTimeout(function() {
-                popupModal.style.display = 'none';
-            }, 250); // Match the duration of the animation
-        });
-    } else {
-        console.log('Popup Modal or continue button not found');
-    }
 
     if (calendlyModal && closeModal) {
         closeModal.addEventListener('click', function() {
@@ -59,9 +46,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     window.addEventListener('click', function(event) {
-        if (event.target === popupModal) {
-            popupModal.style.display = 'none';
-        }
+       
         if (event.target === calendlyModal) {
             calendlyModal.style.display = 'none';
         }
